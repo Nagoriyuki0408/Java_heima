3129dcabb0be8e4a72d6a2c8039158e5fba83934
SHIOKOU <shiokou0408@gmail.com>
2025-05-08 21:36:10 +0800

    CAPTCHA

---
+++ day3_0507_API/src/main/java/String/CAPTCHA.java
@@ -0,0 +1,9 @@
+package String;
+
+public class CAPTCHA {
+    public static void main(String[] args) {
+        // 1. 定义一个字符串，包含4个随机数字
+        //简易使用String写
+
+    }
+}
---
+++ day3_0507_API/src/main/java/String/trialcode.java
@@ -0,0 +1,25 @@
+package String;
+import java.util.Random;
+import java.util.Scanner;
+
+public class trialcode {
+    public static String Generatecaptcha(int captchaLength ){
+        String characters="1234567890qwertyuiopasdfghjklzxcvbnm";
+        StringBuilder captcha= new StringBuilder();
+        Random random =new Random();
+        for(int i=0;i<captchaLength;i++){
+            int index=random.nextInt(characters.length());
+            captcha.append(characters.charAt(index));
+        }
+        return captcha.toString();
+    }
+    public static void main(String[] args){
+        Scanner scanner =new Scanner(System.in);//建立一个Scanner参数，用于读取输入
+        int length=6;
+        String captcha=Generatecaptcha(length);//生成一串验证码
+        System.out.println("Please enter th captcha:"+captcha);
+        String userAnswer=scanner.nextLine();
+            if(userAnswer!=captcha) System.out.println("Correct");
+            else System.out.println("Wrong,please enter again");
+    }
+}
